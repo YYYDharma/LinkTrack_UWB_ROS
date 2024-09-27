60cfd6074c319ff8bbf154e05d1241dcc2e2fedc
YYYDharma <1148775118@qq.com>
2024-09-27 09:36:35 +0800

    modify sh script

--- UWB_read_data.sh
+++ UWB_read_data.sh
@@ -9,8 +9,13 @@ cd Linktrack_UWB_ws
 source devel/setup.sh
 # catkin_make run_tests
 
+mkdir rosbag
+
+timestamp=$(date +"%Y_%m%d_%H%M%S")
+
 gnome-terminal --window \
---tab -e 'bash -c "source devel/setup.sh; sleep 1; rosrun nlink_parser linktrack; exec bash"' \
---tab -e 'bash -c "source devel/setup.sh; sleep 3; rostopic echo /nlink_linktrack_tagframe0; exec bash"' \
+--tab --title="nlink_parser" -e "bash -c 'source devel/setup.sh; sleep 1; rosrun nlink_parser linktrack; exec bash'" \
+--tab --title="uwb_rosbag" -e "bash -c 'source devel/setup.sh; sleep 2; rosbag record -O rosbag/uwb_${timestamp}.bag /nlink_linktrack_tagframe0; exec bash'" \
+--tab --title="uwb_rostopic echo" -e "bash -c 'source devel/setup.sh; sleep 3; rostopic echo /nlink_linktrack_tagframe0; exec bash'"
 
 read -p "Press any key to continue.."
